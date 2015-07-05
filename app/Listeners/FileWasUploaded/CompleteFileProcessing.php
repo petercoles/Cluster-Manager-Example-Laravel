e21a3ec3da89d152f4867665b6a121210342293a
Peter Coles <peter@kuroiweb.com>
2015-07-05 13:07:09 +0100

    remove event var as its not currently used and general tidy up

--- app/Listeners/FileWasUploaded/CompleteFileProcessing.php
+++ app/Listeners/FileWasUploaded/CompleteFileProcessing.php
@@ -9,12 +9,11 @@ class CompleteFileProcessing
     /**
      * Handle the event.
      *
-     * @param  FileWasUploaded  $event
      * @return void
      */
-    public function handle($event)
+    public function handle()
     {
-        // not sure yet exactly waht this would do,
+        // not sure yet exactly what this would do,
         // but it feels as though there's likely to be a tidying up step,
         // so this is a placeholder for it
         Log::info('complete file processing (not yet active)');
